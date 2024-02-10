bb34c501f33a3c3a4062a1280a1428f05f732da0
Laker Turner <la@laker.gay>
2024-02-10 19:33:34 +0000

    no mentions 4 now

--- config/webment_conf.js
+++
@@ -1,6 +0,0 @@
-const pluginWebmentions = require("@chrisburnell/eleventy-cache-webmentions")
-const configWebmentions = require("../src/_data/webmentions.js")
-
-module.exports = function (eleventyConfig) {
-	eleventyConfig.addPlugin(pluginWebmentions, configWebmentions)
-}
