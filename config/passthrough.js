6a8490df1983f0fb2a194a25e6fda3d46c273065
Laker Turner <la@laker.gay>
2024-03-21 12:09:17 +0000

    fix(11ty passthrough): icons was set to the wrong folder

--- config/passthrough.js
+++ config/passthrough.js
@@ -1,6 +1,6 @@
 module.exports = eleventyConfig => {
 	eleventyConfig.addPassthroughCopy("./src/cdn/image");
-	eleventyConfig.addPassthroughCopy({ "./src/cdn/icons": "/" });
+	eleventyConfig.addPassthroughCopy({ "./src/cdn/image/icons": "/" });
 	eleventyConfig.addPassthroughCopy({ "./src/meta/.well-known/": "/.well-known" });
 	eleventyConfig.addPassthroughCopy({ "./src/meta/.well-known/robots.txt": "/robots.txt" });
 	eleventyConfig.addPassthroughCopy({ "./src/meta/manifest.webmanifest": "/manifest.webmanifest" });
